719bde5863a822e21971edb00a005923c4587a09
tolisthanasis <42931603+tolisthanasis@users.noreply.github.com>
2018-09-26 14:32:57 +0200

    Add files via upload

---
+++ DoubleNumbers.java
@@ -0,0 +1,21 @@
+package variableridentifierare;
+
+public class DoubleNumbers {
+	
+	public static void main(String[] args) {
+		double doubleNumber = 10;
+		int integerNumber = 10;
+		
+		boolean equal = doubleNumber == integerNumber;
+		System.out.println(equal);
+        System.out.println(doubleNumber == integerNumber);
+        
+        if (doubleNumber == integerNumber)  {
+        System.out.println("double == integer");
+        } else {
+        	System.out.println("double != integer");
+        }
+	} 
+        
+
+}
