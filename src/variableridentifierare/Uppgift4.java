65f92cbcba7bf753519ed932236fc418155ea256
tolisthanasis <apostollos.thanasis@elev.ga.ntig.se>
2018-10-01 11:27:23 +0200

    m

---
+++ src/variableridentifierare/Uppgift4.java
@@ -0,0 +1,21 @@
+package variableridentifierare;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Uppgift4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in) ;
+	
+		double radius= 7.5;
+		double pi= 3.14;
+		double per= 2*pi*radius;
+		System.out.println(per);
+		
+		
+		double area= pi*radius*radius;
+		System.out.println(area);
+ 
+}
+}
