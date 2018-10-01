63b246f531c930c49f1bd4fd94db0f496c6af5fe
tolisthanasis <apostollos.thanasis@elev.ga.ntig.se>
2018-10-01 11:59:00 +0200

    m

---
+++ src/variableridentifierare/Uppgift6.java
@@ -0,0 +1,16 @@
+package variableridentifierare;
+
+public class Uppgift6 {
+
+	public static void main(String[] args) {
+
+		double width= 5.6;
+		double height= 8.5;		
+		double area= width*height;
+		System.out.println(area);
+		
+		double per= (width*height);
+				System.out.println(per);
+	}
+
+}
