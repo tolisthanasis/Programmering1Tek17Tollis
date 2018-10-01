4ae0f113fe311ce255b69060cdf9f25a4508db5e
tolisthanasis <apostollos.thanasis@elev.ga.ntig.se>
2018-10-01 11:36:04 +0200

    m

---
+++ src/variableridentifierare/Uppgift5.java
@@ -0,0 +1,14 @@
+package variableridentifierare;
+
+public class Uppgift5 {
+
+	public static void main(String[] args) {
+		double k= 9;
+		double l=10;
+		double m=11;
+		double s= ( k+l+m ) /3; 
+		System.out.println(s);
+
+	}
+
+}
